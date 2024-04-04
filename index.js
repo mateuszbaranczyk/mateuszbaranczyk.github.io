e5dabdc27146c258d7b060bcb891615bde53318d
kundzior <grzybek2413@gmail.com>
2024-04-04 16:53:17 +0200

    initial pour

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function calculate () {
   sweetness = document.querySelector('input[name="sweetness"]:checked').value;
   
   getCoffeWeight(weight)
+  createInitialPour(sweetness, weight)
   console.log(weight)
   console.log(strength)
   console.log(sweetness)
@@ -25,17 +26,19 @@ function createInitialPour (sweetness, weight) {
   if (sweetness === "standard") {
     let stPoutr = Math.round(initialWeight * 0.5);
     let ndPour = Math.round(initialWeight * 0.5);
+    insertInitPourHtml(stPoutr, ndPour);
   } else if (sweetness === "sweeter") {
     let stPoutr = Math.round(initialWeight * 0.4);
     let ndPour = Math.round(initialWeight * 0.6);
+    insertInitPourHtml(stPoutr, ndPour);
   } else if (sweetness === "brighter") {
     let stPoutr = Math.round(initialWeight * 0.6);
     let ndPour = Math.round(initialWeight * 0.4);
-  }
-
-  insertInitPourHtml(stPoutr, ndPour)
+    insertInitPourHtml(stPoutr, ndPour);
+  }  
 }
 
 function insertInitPourHtml (stPoutr, ndPour) {
-
+  document.getElementById("init").innerHTML += "<p> 1st pour "  + Math.round(stPoutr) + " grams of water.</p>";
+  document.getElementById("init").innerHTML += "<p> 2nd pour " + Math.round(ndPour) + " grams of water.</p>";
 }
