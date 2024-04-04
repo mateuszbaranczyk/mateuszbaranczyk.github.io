ad32b130ba5bcc93cb2c03b7e6570f62aaa30442
kundzior <grzybek2413@gmail.com>
2024-04-04 18:40:28 +0200

    timer js

---
+++ timer.js
@@ -0,0 +1,23 @@
+const countdownEl = document.querySelector(".countdown");
+const progressBarEl = document.querySelector(".progress");
+
+let remainingTime = 60; // seconds
+const totalTime = remainingTime;
+
+function countdown() {
+  if (remainingTime > 0) {
+    // update countdown timer
+    countdownEl.textContent = remainingTime;
+
+    // update progress bar
+    const progress = ((totalTime - remainingTime) / totalTime) * 100;
+    progressBarEl.style.width = `${progress}%`;
+
+    remainingTime--;
+    setTimeout(countdown, 1000);
+  } else {
+    // countdown finished
+    progressBarEl.style.width = "100%";
+    countdownEl.textContent = "Time's up!";
+  }
+}
