38db6969d0171bda72e22e80afd248c91c67047e
kundzior <grzybek2413@gmail.com>
2024-04-04 16:17:02 +0200

    needed coffe and init pour

--- index.js
+++ index.js
@@ -3,11 +3,39 @@ let strength
 let sweetness
 
 function calculate () {
-    weight = document.getElementById("weight").value;
-    strength = document.querySelector('input[name="strength"]:checked').value;
-    sweetness = document.querySelector('input[name="sweetness"]:checked').value;
+  weight = document.getElementById("weight").value;
+  strength = document.querySelector('input[name="strength"]:checked').value;
+  sweetness = document.querySelector('input[name="sweetness"]:checked').value;
+  
+  getCoffeWeight(weight)
+  console.log(weight)
+  console.log(strength)
+  console.log(sweetness)
+}
 
-    console.log(weight)
-    console.log(strength)
-    console.log(sweetness)
+function getCoffeWeight (weight) {
+  let coffeWeight = Number(weight) / 15;
+  document.getElementById("coffe-weight").innerHTML += "You'll need " + Math.round(coffeWeight) + " grams of coffee.";
+  
+}
+
+function createInitialPour (sweetness, weight) {
+  let initialWeight = weight * 0.4;
+
+  if (sweetness === "standard") {
+    let stPoutr = Math.round(initialWeight * 0.5);
+    let ndPour = Math.round(initialWeight * 0.5);
+  } else if (sweetness === "sweeter") {
+    let stPoutr = Math.round(initialWeight * 0.4);
+    let ndPour = Math.round(initialWeight * 0.6);
+  } else if (sweetness === "brighter") {
+    let stPoutr = Math.round(initialWeight * 0.6);
+    let ndPour = Math.round(initialWeight * 0.4);
+  }
+
+  insertInitPourHtml(stPoutr, ndPour)
+}
+
+function insertInitPourHtml (stPoutr, ndPour) {
+
 }
