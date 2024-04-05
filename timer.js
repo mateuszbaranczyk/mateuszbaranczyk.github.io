6aac9079bad77e6eede2db69d775761c74111ae5
kundzior <grzybek2413@gmail.com>
2024-04-05 12:47:20 +0200

    timer

--- timer.js
+++ timer.js
@@ -1,23 +1,69 @@
-const countdownEl = document.querySelector(".countdown");
-const progressBarEl = document.querySelector(".progress");
+window.onload = function () {
+  
+  var seconds = 0o0; 
+  var tens = 0o0; 
+  var minutes = 0o0;
+  var appendTens = document.getElementById("tens");
+  var appendSeconds = document.getElementById("seconds");
+  var appendMinutes = document.getElementById("minutes")
+  var buttonStart = document.getElementById('button-start');
+  var buttonStop = document.getElementById('button-stop');
+  var buttonReset = document.getElementById('button-reset');
+  var Interval ;
 
-let remainingTime = 210; // seconds
-const totalTime = remainingTime;
-
-function countdown() {
-  if (remainingTime > 0) {
-    // update countdown timer
-    countdownEl.textContent = remainingTime;
-
-    // update progress bar
-    const progress = ((totalTime - remainingTime) / totalTime) * 100;
-    progressBarEl.style.width = `${progress}%`;
-
-    remainingTime--;
-    setTimeout(countdown, 1000);
-  } else {
-    // countdown finished
-    progressBarEl.style.width = "100%";
-    countdownEl.textContent = "Time's up!";
+  buttonStart.onclick = function() {
+    
+    clearInterval(Interval);
+     Interval = setInterval(startTimer, 10);
   }
+  
+    buttonStop.onclick = function() {
+       clearInterval(Interval);
+  }
+  
+
+  buttonReset.onclick = function() {
+     clearInterval(Interval);
+    tens = "00";
+  	seconds = "00";
+    minutes = "00";
+    appendTens.innerHTML = tens;
+  	appendSeconds.innerHTML = seconds;
+  }
+  
+   
+  
+  function startTimer () {
+    tens++; 
+    
+    if(tens <= 9){
+      appendTens.innerHTML = "0" + tens;
+    }
+    
+    if (tens > 9){
+      appendTens.innerHTML = tens;
+      
+    } 
+    
+    if (tens > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+    
+    if (seconds > 9){
+      appendSeconds.innerHTML = seconds;
+    }
+
+    if (seconds > 59) {
+      minutes ++;
+      seconds = 0;
+      appendMinutes.innerHTML = "0" + minutes;
+    }
+  
+  }
+  
+
 }
